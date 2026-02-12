e7dc1966114204f96deb080427a53e0c5dd3fc64
Ye Wint Thu <40794501@live.napier.ac.uk>
2026-02-12 23:49:30 +0700

    Footer
    Signed-off-by: Ye Wint Thu <40794501@live.napier.ac.uk>

---
+++ docs/footer.php
@@ -0,0 +1,73 @@
+<footer class="footer">
+    <div class="footer-container">
+
+        <div class="footer-col">
+            <h3>Quick Links</h3>
+            <a href="#">Shop</a>
+            <a href="#">Wholesale</a>
+            <a href="#">About Us</a>
+            <a href="#">Contact Us</a>
+        </div>
+
+        <div class="footer-col">
+            <h3>Support</h3>
+            <a href="#">Warranty & FAQs</a>
+            <a href="#">Reservation Policy</a>
+            <a href="#">Delivery Policy</a>
+            <a href="#">Payment Information</a>
+        </div>
+
+        <div class="footer-col">
+            <h3>Media</h3>
+            <a href="#">Unboxing Videos</a>
+            <a href="#">Influencers’ Reviews</a>
+        </div>
+
+        <div class="footer-col contact">
+            <h3>Contact</h3>
+
+            <div class="contact-info">
+                <div class="row">
+                    <span class="label">Address:</span>
+                    <span>
+                        No. 112, 52nd Street, Middle Block, Pazundaung Township, Yangon 11171
+                    </span>
+                </div>
+
+                <div class="row">
+                    <span class="label">Phone:</span>
+                    <span>09-251562642, 09-424574187</span>
+                </div>
+
+                <div class="row">
+                    <span class="label">Email:</span>
+                    <span>zyppcamerahouse2023@gmail.com</span>
+                </div>
+            </div>
+
+
+            <div class="social-icons">
+                <a href="#" class="social-link fb" aria-label="Facebook">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+
+                <a href="#" class="social-link tt" aria-label="TikTok">
+                    <i class="fab fa-tiktok"></i>
+                </a>
+
+                <a href="#" class="social-link tg" aria-label="Telegram">
+                    <i class="fas fa-paper-plane"></i>
+                </a>
+
+                <a href="#" class="social-link ig" aria-label="Instagram">
+                    <i class="fab fa-instagram"></i>
+                </a>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="footer-bottom">
+        © ZYPP Camera House
+    </div>
+</footer>
--- docs/index.php
+++ docs/index.php
@@ -4,11 +4,15 @@
     <?php include 'head.php'; ?>
     <!-- css links -->
     <link rel="stylesheet" href="./assets/css/navbar.css">
+    <link rel="stylesheet" href="./assets/css/footer.css">
 </head>
 <body>
     <!--navbar & sidebar -->
     <?php include 'navbar.php'; ?>
-
+    <br><br><br><br><br>
+    <br><br><br><br><br>
+    <br><br><br><br><br>
+    <?php include 'footer.php'; ?>
     <script src="./assets/js/navbar.js"></script>
 </body>
 </html>
